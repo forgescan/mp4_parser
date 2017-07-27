ca3342c0229e391caf767e8fba327a77880416fc
forgescan <forgescan@gmail.com>
2017-07-27 12:42:24 -0400

    test mp4 parser

---
+++ server.js
@@ -0,0 +1,9 @@
+﻿var http = require('http');
+var express = require('express');
+var app = express();
+var port = process.env.port || 1337;
+var server = require('http').Server(app);
+
+server.listen(port);
+app.use(express.static(__dirname + '/static'));
+
